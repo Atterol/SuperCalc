a199133b7ce792664be55d8f172415b28cafb77b
Loretta <loretta.giannino@gmail.com>
2019-05-02 10:24:32 +0200

    Initial

---
+++ SuperCalculator/src/it/rgi/supercalc/SuperCalc.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package it.rgi.supercalc;
+
+/**
+ * @author academy1426r.01
+ *
+ */
+public class SuperCalc {
+
+	/**
+	 * 
+	 */
+	private int ret; 
+	
+	public SuperCalc() {
+	}
+	
+	public int sum(int op1, int op2) {
+		ret= op1+op2;
+		return ret;
+	}
+
+	public int sub(int op1,int op2) {
+		ret= op1-op2;
+		return ret;
+	}
+	
+	public int mul(int op1,int op2) {
+		ret= op1*op2;
+		return ret;
+	}
+}
---
+++ SuperCalculator/src/it/rgi/supercalc/SuperCalcTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package it.rgi.supercalc;
+
+/**
+ * @author academy1426r.01
+ *
+ */
+public class SuperCalcTest {
+
+	/**
+	 * 
+	 */
+	public SuperCalcTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SuperCalc sc = new SuperCalc();
+		
+		int op1 = 20, op2= 30;
+		int sum = sc.sum(op1,op2);
+		int sub = sc.sub(op1,op2);
+		
+		System.out.println("Somma : " + op1 + " + " + op2 + " = " + sum);
+		System.out.println("Sottrazione : " + op1 + " - " + op2 + " = " + sub);
+	}
+
+}
